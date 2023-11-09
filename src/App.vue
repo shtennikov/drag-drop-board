30603a46bd0fb8868bf30c8974ce8041d4f9144d
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-09 18:03:41 +0500

    init: start project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum incidunt repudiandae suscipit, asperiores sapiente
+        eos atque nobis fugiat aperiam accusamus quas eligendi libero dignissimos quae. Ducimus nisi corporis magni
+        dolorum.
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
