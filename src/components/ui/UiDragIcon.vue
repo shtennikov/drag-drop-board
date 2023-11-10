6e488907d36e7a5d8559f4f8bd3f0beff2e04e90
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-10 12:50:17 +0500

    feat: add UiDragIcon component

---
+++ src/components/ui/UiDragIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="drag_icon"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.drag_icon {
+    width: 16px;
+    height: 16px;
+    background-image: url(../../assets/images/interface-essential-menu-3.svg);
+    cursor: move;
+}
+</style>
